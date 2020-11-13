5c1069613c6f33f7ea6634ea65e1c699d3fae6e9
Martin Kubica <martinko29.km@gmail.com>
2020-11-13 10:30:40 +0100

    Add project files.

---
+++ Open-Lab-10.01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Open_Lab_10._00
+{
+    public class Book
+    {
+        public string title;
+        private int pages;
+
+        public void Pepega(int j)
+        {
+            pages = j;
+            Console.WriteLine(pages);
+        }
+
+
+    }
+        class Program
+        {
+
+        static void Main(string[] args)
+        {
+
+            Book LOTR = new Book();
+            LOTR.title = "Lord of the Rings";
+            
+
+            Console.WriteLine(LOTR.title);
+            LOTR.Pepega(16);
+
+        }
+
+        }
+        
+
+
+
+
+    
+}
